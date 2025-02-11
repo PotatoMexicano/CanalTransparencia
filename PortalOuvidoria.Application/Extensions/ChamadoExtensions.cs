8cec771b26e36ae2e3c613b5856d3a4516843c34
Gabriel Cordeiro <potatomexicano006@yahoo.com>
2025-02-11 16:34:00 -0300

    feat: Adicionado service para chamados.
    * Adicionado conversão dos chamados para DTO.

---
+++ PortalOuvidoria.Application/Extensions/ChamadoExtensions.cs
@@ -0,0 +1,22 @@
+﻿using PortalOuvidoria.Domain.DTOs.Chamado;
+using PortalOuvidoria.Domain.Entities;
+
+namespace PortalOuvidoria.Application.Extensions;
+public static class ChamadoExtensions
+{
+    public static ChamadoDTO ToDTO(this Chamado entity)
+    {
+        return new ChamadoDTO
+        {
+            ComentarioFinalizado = entity.ComentarioFinalizado,
+            IdSituacao = entity.IdSituacao,
+            TokenAcompanhamento = entity.TokenAcompanhamento,
+            UTC_DataAnalise = entity.UTC_DataAnalise,
+            UTC_DataComentario = entity.UTC_DataComentario,
+            UTC_DataEvidencia = entity.UTC_DataEvidencia,
+            UTC_DataFinalizado = entity.UTC_DataFinalizado,
+            UTC_DataRegistro = entity.UTC_DataRegistro,
+            Situacao = Domain.Constants.Situacao.GetNome(entity.IdSituacao)
+        };
+    }
+}
---
+++ PortalOuvidoria.Application/Services/ChamadoService.cs
@@ -0,0 +1,37 @@
+﻿using MimeTypes;
+using PortalOuvidoria.Application.Extensions;
+using PortalOuvidoria.Domain.DTOs.Chamado;
+using PortalOuvidoria.Domain.Entities;
+using PortalOuvidoria.Domain.Interfaces;
+
+namespace PortalOuvidoria.Application.Services;
+public class ChamadoService(IChamadoRepository repository) : IChamadoService
+{
+    public async Task<ChamadoDTO?> ObterChamado(String token, CancellationToken cancellation)
+    {
+        Chamado? chamado = await repository.ObterChamadoAsync(token, cancellation);
+
+        if (chamado == null) return null;
+
+        return chamado.ToDTO();
+    }
+
+    public async Task<ChamadoDTO> Registrar(RegistrarChamadoDTO request, CancellationToken cancellation)
+    {
+        Chamado chamado = new Chamado(request.Assunto, request.Mensagem);
+
+        if (!String.IsNullOrEmpty(request.File))
+        {
+            Byte[] fileBytes = Convert.FromBase64String(request.File);
+            String fileType = MimeTypeMap.GetExtension(request.MimeType);
+
+            String fileName = Path.Combine("uploads", $"{chamado.TokenAcompanhamento}{fileType}");
+            File.WriteAllBytes(fileName, fileBytes);
+        }
+
+        Chamado resultado = await repository.RegisterAsync(chamado, cancellation);
+
+        return resultado.ToDTO();
+
+    }
+}
